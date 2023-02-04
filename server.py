5733a5dd4adb1a43baa2e4fa6bd379b597ce4596
Serghei Iakovlev <egrep@protonmail.ch>
2023-02-04 15:08:00 +0100

    Provide /v1/products route

--- server.py
+++ server.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask import json
+
+
 app = Flask(__name__)
 
-@app.route('/')
+
+def get_products_list():
+    with open('products.json') as f:
+        return json.loads(f.read())
+
+
+@app.route('/v1/products')
 def index():
-    return 'Web App with Python Flask!'
+    data = get_products_list()
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
