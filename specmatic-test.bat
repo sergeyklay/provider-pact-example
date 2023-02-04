2f45bfda92e70d83e81dab7611af470bef76f958
Serghei Iakovlev <egrep@protonmail.ch>
2023-02-04 14:14:25 +0100

    Use latest specmatic

--- specmatic-test.bat
+++
@@ -1,1 +0,0 @@
-java -jar "C:\hostedtoolcache\windows\specmatic\0.59.0\x64\specmatic.jar" %*
--- specmatic-test.ps1
+++
@@ -1,3 +0,0 @@
-Start-Process -FilePath java `
--ArgumentList '-jar C:\hostedtoolcache\windows\specmatic\0.59.0\x64\specmatic.jar --version' `
--PassThru
