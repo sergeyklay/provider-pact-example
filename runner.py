a3df882ca38b27cfe2159021185f3689c291cdb6
Serghei Iakovlev <egrep@protonmail.ch>
2023-02-21 23:50:47 +0100

    Disable unconditional migration in startup entrypoint

--- runner.py
+++ runner.py
@@ -19,16 +19,39 @@ To run this entrypoint use any of the following command:
 
 import os
 
-from flask_migrate import upgrade
-
-from products.app import create_app, db, load_env_vars
-from products.seeder import seed_products
+from products.app import create_app, load_env_vars
 
 load_env_vars(os.path.dirname(os.path.abspath(__file__)))
 
-config = os.getenv('FLASK_CONFIG', 'default').lower()
+# The application supports different startup modes:
+#    - development
+#    - testing
+#    - production
+#    - default (alias for development)
+#
+# To specify an application's startup mode, set the variable
+# PRODUCTS_API_CONFIG to an appropriate value as follows:
+#
+#   $ export PRODUCTS_API_CONFIG=production
+#   $ flask --app runner:app run
+#
+# Please note: When starting the application with flask script
+# (which is by design the way to run an application in development mode),
+# the --debug command line flag has nothing to do with the value of
+# PRODUCTS_API_CONFIG variable because they are handled differently, by
+# different subsystems. Thus, the following way of starting is perfectly
+# functional (although it makes no sense):
+#
+#   $ export PRODUCTS_API_CONFIG=production
+#   $ flask --app runner:app run --debug
+#
+config = os.getenv('PRODUCTS_API_CONFIG', 'default').lower()
 app = create_app(config)
 
+# The alternative way to start the application is through the Flask.run()
+# method. This will immediately launch a local server exactly the same way
+# the flask script does.
+#
 # The following code is for starting an application using only
 # one of the following launch methods:
 #
@@ -40,15 +63,4 @@ app = create_app(config)
 #   $ flask --app runner:app run
 #
 if __name__ == '__main__':
-    if config == 'testing':
-        # Add seed data to the database if current mode is testing
-        with app.app_context():
-            db.session.remove()
-            db.drop_all()
-            upgrade()
-            seed_products()
-
-    # The alternative way to start the application is through the Flask.run()
-    # method. This will immediately launch a local server exactly the same way
-    # the flask script does.
     app.run()
