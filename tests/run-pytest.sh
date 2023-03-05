39028b05ac7a976d33680c85f24d4dd754d19b0b
Serghei Iakovlev <egrep@protonmail.ch>
2023-03-06 00:56:42 +0100

    test: speed up pacts verifier shell runner

--- tests/run-pytest.sh
+++ tests/run-pytest.sh
@@ -15,9 +15,11 @@ else
   python="${PYTHON:-$(pyenv which python 2>/dev/null)}"
 fi
 
+PORT=5001
+
 # Run the Flask server, using the 'tests/app.py' as the app to be able to
 # inject the '-pact/provider-states' endpoint
-FLASK_APP=tests/app.py $python -m flask run -p 5001 &
+FLASK_APP=tests/app.py $python -m flask run -p $PORT &
 FLASK_PID=$!
 
 # Make sure the Flask server is stopped when finished to avoid blocking the port
@@ -28,7 +30,13 @@ function teardown {
 trap teardown EXIT
 
 # Wait a little in case Flask isn't quite ready
-sleep 1
+while ! nc -z localhost $PORT; do
+  sleep 0.1
+done
+
+# Turn off the annoying warning in the output
+PACT_DO_NOT_TRACK=true
+export PACT_DO_NOT_TRACK
 
 # Now run the tests
 $python -m pytest tests --verbose -o log_cli=true -m contracts
